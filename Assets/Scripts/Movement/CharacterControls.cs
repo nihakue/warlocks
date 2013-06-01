ec39d1c27bbf2e0f4ecb4976c824d5cc2898c5c3
nihakue <walden.gabrielw@gmail.com>
2013-06-01 03:09:19 -0600

    added some gamepad support

--- Assets/Scripts/Movement/CharacterControls.cs
+++ Assets/Scripts/Movement/CharacterControls.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class CharacterControls : MonoBehaviour {
 	
-
+	public enum CONTROL_TYPE {Gamepad, Mouse};
+	
+	public CONTROL_TYPE controlType;
 	private Rigidbody rb;
 	private float squaredLockSpeed;
 	private float squaredMagnitude;
@@ -34,40 +36,92 @@ public class CharacterControls : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-	
-		if (Physics.Raycast(ray, out rayHit))
+		//TO DO: This control type may be a hack
+			if (controlType == CONTROL_TYPE.Mouse)
+			{
+			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		
+			if (Physics.Raycast(ray, out rayHit))
+			{
+				Transform tempTran = transform;
+				tempTran.LookAt(rayHit.point);
+				transform.rotation = Quaternion.Euler(0, tempTran.rotation.eulerAngles.y, 0);
+			}
+			
+			if (Input.GetButtonDown("Movement")){
+				SetMoveTowards(Input.mousePosition);
+			}
+		
+		
+			if (Input.GetButtonDown("Fire1")){
+				if (GUIUtility.hotControl == 0){
+						spellSelector.CastSpell(lockStatus.currentSpell);
+				}
+			}
+			
+			if (Input.GetButtonDown("Spell1")){
+				CastSpell1();
+			}
+			else if (Input.GetButtonDown("Spell2")){
+				CastSpell2();
+			}
+			else if (Input.GetButtonDown("Spell3")){
+				CastSpell3();
+			}
+			else if (Input.GetButtonDown("Spell4")){
+				CastSpell4();
+			}
+			else if (Input.GetButtonDown ("Spell5")){
+				CastSpell5();
+			}
+			
+		}
+		
+		if (controlType == CONTROL_TYPE.Gamepad)
 		{
-			Transform tempTran = transform;
-			tempTran.LookAt(rayHit.point);
-			transform.rotation = Quaternion.Euler(0, tempTran.rotation.eulerAngles.y, 0);
-		}
-		
-		if (Input.GetButtonDown("Movement")){
-			SetMoveTowards(Input.mousePosition);
-		}
-		
-		if (Input.GetButtonDown("Fire1")){
-			if (GUIUtility.hotControl == 0){
-					spellSelector.CastSpell(lockStatus.currentSpell);
+				
+			if (Input.GetAxis("Horizontal") != 0)
+			{
+				float axisValue = Input.GetAxis("Horizontal");
+				rigidbody.AddForce(new Vector3(lockSpeed*axisValue, 0, 0));
+			}
+			
+			if (Input.GetAxis("Vertical") != 0)
+			{
+				float axisValue = Input.GetAxis("Vertical");
+				rigidbody.AddForce(new Vector3(0, 0, lockSpeed*axisValue));
+			}
+			//TODO: Fixed this shit
+			if (Input.GetAxis("HorizontalLook") != 0 || Input.GetAxis("VerticalLook") != 0)
+			{
+				float xAxisValue = Input.GetAxis("HorizontalLook");
+				float yAxisValue = Input.GetAxis("VerticalLook");
+				float radian = Mathf.Asin(xAxisValue);
+				float degrees = radian * Mathf.Rad2Deg;
+				if (yAxisValue > 0)
+					degrees = 360 - degrees;
+				
+				Debug.Log(degrees);
+				Quaternion direction = Quaternion.Euler(0, degrees, 0);
+				transform.rotation = direction;
+				
+			}
+			
+			
+			if (Input.GetButtonDown("Fire2")){
+				CastSpell1();
+			}
+			
+			if (Input.GetButtonDown("Fire3")){
+				CastSpell2();
+			}
+			
+			if (Input.GetButtonDown("Fire4")){
+				CastSpell3();
 			}
 		}
 		
-		if (Input.GetButtonDown("Spell1")){
-			CastSpell1();
-		}
-		else if (Input.GetButtonDown("Spell2")){
-			CastSpell2();
-		}
-		else if (Input.GetButtonDown("Spell3")){
-			CastSpell3();
-		}
-		else if (Input.GetButtonDown("Spell4")){
-			CastSpell4();
-		}
-		else if (Input.GetButtonDown ("Spell5")){
-			CastSpell5();
-		}
+		
 		/*
 		if (Input.GetKey(KeyCode.D))
 			rigidbody.AddForce(new Vector3(lockSpeed, 0, 0));
@@ -95,8 +149,8 @@ public class CharacterControls : MonoBehaviour {
 	void FixedUpdate(){
 		
 		//Moves the lock towards the last clicked point
-		if (!((moveTowards - transform.position).sqrMagnitude < 1 ))
-			MoveTowards();
+		//if (!((moveTowards - transform.position).sqrMagnitude < 1 ))
+		//	MoveTowards();
 
 		
 		//Limits the velocity of the lock to it's max speed
@@ -132,32 +186,66 @@ public class CharacterControls : MonoBehaviour {
 	
 	//----------------------Spell Casters------------------------//
 	
+	
 	void CastSpell1(){
-		lockStatus.currentSpell = (int)SPELLS.Fireball;
-		gui.SetProjectileCursor();
-		if (Input.GetKey(KeyCode.LeftAlt))
+		
+		if (controlType == CONTROL_TYPE.Mouse)
+		{
+			lockStatus.currentSpell = (int)SPELLS.Fireball;
+			gui.SetProjectileCursor();
+			if (Input.GetKey(KeyCode.LeftAlt))
+				spellSelector.smartCast = true;
+		}
+		else if (controlType == CONTROL_TYPE.Gamepad)
+		{
+			lockStatus.currentSpell = (int)SPELLS.Fireball;
 			spellSelector.smartCast = true;
+		}
 	}
 	
 	void CastSpell2(){
-		lockStatus.currentSpell = (int)SPELLS.Fireblast;
-		gui.SetProjectileCursor();
-		if (Input.GetKey(KeyCode.LeftAlt))
+		if (controlType == CONTROL_TYPE.Mouse)
+		{
+			lockStatus.currentSpell = (int)SPELLS.Fireblast;
+			gui.SetProjectileCursor();
+			if (Input.GetKey(KeyCode.LeftAlt))
+				spellSelector.smartCast = true;
+		}
+		else if (controlType == CONTROL_TYPE.Gamepad)
+		{
+			lockStatus.currentSpell = (int)SPELLS.Fireblast;
 			spellSelector.smartCast = true;
+		}
 	}
 	
 	void CastSpell3(){
-		lockStatus.currentSpell = (int)SPELLS.Teleport;
-		gui.SetProjectileCursor();
-		if (Input.GetKey(KeyCode.LeftAlt))
+		if (controlType == CONTROL_TYPE.Mouse)
+		{
+			lockStatus.currentSpell = (int)SPELLS.Teleport;
+			gui.SetProjectileCursor();
+			if (Input.GetKey(KeyCode.LeftAlt))
+				spellSelector.smartCast = true;
+		}
+		else if (controlType == CONTROL_TYPE.Gamepad)
+		{
+			lockStatus.currentSpell = (int)SPELLS.Teleport;
 			spellSelector.smartCast = true;
+		}
 	}
 	
 	void CastSpell4(){
-		lockStatus.currentSpell = (int)SPELLS.Windblast;
-		gui.SetProjectileCursor();
-		if (Input.GetKey(KeyCode.LeftAlt))
+		if (controlType == CONTROL_TYPE.Mouse)
+		{
+			lockStatus.currentSpell = (int)SPELLS.Windblast;
+			gui.SetProjectileCursor();
+			if (Input.GetKey(KeyCode.LeftAlt))
+				spellSelector.smartCast = true;
+		}
+		else if (controlType == CONTROL_TYPE.Gamepad)
+		{
+			lockStatus.currentSpell = (int)SPELLS.Windblast;
 			spellSelector.smartCast = true;
+		}
 	}
 	
 	void CastSpell5(){
